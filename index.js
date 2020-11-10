219534bb215b34b59a1d9d96de99a6cfcb9a6d0a
HuffDaddy0 <ty.huff24@gmail.com>
2020-11-10 16:06:02 -0500

    commit tree 65801f3ea220032ad271636bcd6ebf2d00879d79

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+function saturdayFun(thing='roller-skate'){
+    return `This Saturday, I want to ${thing}!`
+};
+
+let mondayWork = function(work='go to the office'){
+return `This Monday, I will ${work}.`
+};
+
+function wrapAdjective(whatever='*'){
+    return function(thing='special'){
+        return `You are ${whatever}${thing}${whatever}!`
+    }
+}
+
+const Calculator = {
+    add: function(){return 1 + 3},
+    subtract: function(){return 1 - 3},
+    multiply: function(){return 1 * 3},
+    divide: function(){return 10/5}    
+}
